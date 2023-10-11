f6148bd1d2cf83fe5a6e9a5d1a3703cb6e8eb3f2
Fraser Dempster <fraserdempster23@gmail.com>
2023-10-11 19:11:17 +0100

    added basic routing

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/pages/HomeScreen'; // Import your HomeScreen component
+import SettingsScreen from './src/pages/SettingsScreen'; // Import your SettingsScreen component
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
